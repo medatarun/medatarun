57114c8538322c003a4d71594cb2db5a6a615311
Sebastien Just <sebastien.just@seij.net>
2025-10-27 08:34:03 +0100

    Fill tests for Json model storage

--- extensions/modeljson/src/test/kotlin/io/medatarun/ext/modeljson/ModelJsonRepositoryTest.kt
+++ extensions/modeljson/src/test/kotlin/io/medatarun/ext/modeljson/ModelJsonRepositoryTest.kt
@@ -24,12 +24,20 @@ class ModelJsonRepositoryTest {
         }
     }
 
+    // ------------------------------------------------------------------------
+    // Matching
+    // ------------------------------------------------------------------------
+
     @Test
     fun testMatches() {
         val env = TestEnv()
         assertFalse(env.repo.matchesId(ModelRepositoryId("abc")))
         assertTrue(env.repo.matchesId(ModelJsonRepository.REPOSITORY_ID))
     }
+
+    // ------------------------------------------------------------------------
+    // Find
+    // ------------------------------------------------------------------------
 
     @Test
     fun `findModelById not found`() {
@@ -52,22 +60,49 @@ class ModelJsonRepositoryTest {
         assertEquals(env.sampleModel2.id, m2.id)
     }
 
+    // ------------------------------------------------------------------------
+    // Models
+    // ------------------------------------------------------------------------
+
     @Test
     fun `create then path is correct`() {
         val env = TestEnv()
         env.repo.createModel(env.sampleModel)
         env.repo.createModel(env.sampleModel2)
-        val path1 = env.fs.modelsDirectory().resolve(env.sampleModel.id.value+".json")
+        val path1 = env.fs.modelsDirectory().resolve(env.sampleModel.id.value + ".json")
         assertTrue(path1.exists())
-        val path2 = env.fs.modelsDirectory().resolve(env.sampleModel2.id.value+".json")
+        val path2 = env.fs.modelsDirectory().resolve(env.sampleModel2.id.value + ".json")
         assertTrue(path2.exists())
+    }
+
+    @Test
+    fun `findAllModelIds empty`() {
+        val env = TestEnv()
+        val ids = env.repo.findAllModelIds()
+        assertTrue(ids.isEmpty())
 
     }
 
     @Test
-    fun `update id and path changed`() {
-        TODO("Not yet implemented")
+    fun `findAllModelIds one model`() {
+        val env = TestEnv()
+        env.importSample()
+        val ids = env.repo.findAllModelIds()
+        assertEquals(1, ids.size)
+        assertTrue(ids.contains(env.sampleModel.id))
     }
+
+    @Test
+    fun `findAllModelIds two models`() {
+        val env = TestEnv()
+        env.importSample()
+        env.importSample2()
+        val ids = env.repo.findAllModelIds()
+        assertEquals(2, ids.size)
+        assertTrue(ids.contains(env.sampleModel.id))
+        assertTrue(ids.contains(env.sampleModel2.id))
+    }
+
 
     @Test
     fun `update name`() {
@@ -80,13 +115,18 @@ class ModelJsonRepositoryTest {
     }
 
     @Test
+    fun `update version`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
     fun `delete then no file left`() {
         val env = TestEnv()
         env.repo.createModel(env.sampleModel)
         env.repo.createModel(env.sampleModel2)
         env.repo.deleteModel(env.sampleModel.id)
-        val path1 = env.fs.modelsDirectory().resolve(env.sampleModel.id.value+".json")
-        val path2 = env.fs.modelsDirectory().resolve(env.sampleModel2.id.value+".json")
+        val path1 = env.fs.modelsDirectory().resolve(env.sampleModel.id.value + ".json")
+        val path2 = env.fs.modelsDirectory().resolve(env.sampleModel2.id.value + ".json")
         assertFalse(path1.exists())
         assertTrue(path2.exists())
         env.repo.deleteModel(env.sampleModel2.id)
@@ -94,5 +134,98 @@ class ModelJsonRepositoryTest {
 
 
     }
+    // ------------------------------------------------------------------------
+    // Types
+    // ------------------------------------------------------------------------
+
+
+    @Test
+    fun `create type`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `update type name`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `update type description`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `delete type`() {
+        TODO("Not yet implemented")
+    }
+    // ------------------------------------------------------------------------
+    // Entities
+    // ------------------------------------------------------------------------
+
+
+    @Test
+    fun `update entity def id`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `update entity def name`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `update entity def description`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `update entity def identifier attribute`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `delete entity def`() {
+        TODO("Not yet implemented")
+    }
+
+    // ------------------------------------------------------------------------
+    // Attributes
+    // ------------------------------------------------------------------------
+
+    @Test
+    fun `create attribute def`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `update attribute def id`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `update attribute def name`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `update attribute def description`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `update attribute def type`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `update attribute def optional`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `delete attribute def`() {
+        TODO("Not yet implemented")
+    }
+
 }
 
