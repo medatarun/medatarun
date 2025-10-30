0c2bef18c3c15e1ad86297e87a65a0e07f2141d4
Sebastien Just <sebastien.just@seij.net>
2025-10-30 08:32:29 +0100

    Datapackage import

--- extensions/frictionlessdata/src/test/kotlin/io/medatarun/ext/frictionlessdata/TableSchemaTest.kt
+++ extensions/frictionlessdata/src/test/kotlin/io/medatarun/ext/frictionlessdata/TableSchemaTest.kt
@@ -1,22 +1,17 @@
 package io.medatarun.ext.frictionlessdata
 
-import io.medatarun.model.infra.AttributeDefInMemory
-import io.medatarun.model.infra.EntityDefInMemory
 import io.medatarun.model.infra.ModelHumanPrinterEmoji
-import io.medatarun.model.infra.ModelInMemory
-import io.medatarun.model.model.AttributeDefId
-import io.medatarun.model.model.EntityDefId
-import io.medatarun.model.model.LocalizedTextNotLocalized
-import io.medatarun.model.model.ModelHumanPrinter
-import io.medatarun.model.model.ModelId
-import io.medatarun.model.model.ModelTypeId
-import io.medatarun.model.model.ModelVersion
 import io.medatarun.model.ports.ResourceLocator
 import org.junit.jupiter.api.Test
+import java.net.URI
 
 class TableSchemaTest {
 
     val conv = FrictionlessConverter()
+
+    /**
+     * Resource locator for tests that map file paths to test resources.
+     */
     class TestResourceLocator(val path: String) : ResourceLocator {
         override fun getRootContent(): String {
             return this::class.java.getResource(path).readText()
@@ -31,11 +26,60 @@ class TableSchemaTest {
         }
 
     }
+
+    /**
+     * Fake resource locator to map URLs to files in test resources to avoid getting files on internet during tests.
+     * This allows tests on datapackage to simulate content fetching on a limited set on URLs
+     */
+    class TestResourceLocatorURL(val baseURI: URI) : ResourceLocator {
+
+
+        val map = mapOf(
+            "https://raw.githubusercontent.com/cnigfr/schema-paysage/refs/heads/main/datapackage.json" to "/datapackage-paysage.json",
+            "https://raw.githubusercontent.com/cnigfr/schema-paysage/refs/heads/main/schema/classe-atlaspaysager/schema.json" to "/datapackage-paysage-classe-atlaspaysager.json",
+            "https://raw.githubusercontent.com/cnigfr/schema-paysage/refs/heads/main/schema/classe-documentpaysage/schema.json" to "/datapackage-paysage-classe-documentpaysage.json",
+            "https://raw.githubusercontent.com/cnigfr/schema-paysage/refs/heads/main/schema/classe-dynamique/schema.json" to "/datapackage-paysage-classe-dynamique.json",
+            "https://raw.githubusercontent.com/cnigfr/schema-paysage/refs/heads/main/schema/classe-ensemblepaysager/schema.json" to "/datapackage-paysage-classe-ensemblepaysager.json",
+            "https://raw.githubusercontent.com/cnigfr/schema-paysage/refs/heads/main/schema/classe-limitedecoupagepaysager/schema.json" to "/datapackage-paysage-classe-limitedecoupagepaysager.json",
+            "https://raw.githubusercontent.com/cnigfr/schema-paysage/refs/heads/main/schema/classe-sousunitepaysagere/schema.json" to "/datapackage-paysage-classe-sousunitepaysagere.json",
+            "https://raw.githubusercontent.com/cnigfr/schema-paysage/refs/heads/main/schema/classe-unitepaysagere/schema.json" to "/datapackage-paysage-classe-unitepaysagere.json",
+        )
+
+
+        override fun getRootContent(): String {
+            return this::class.java.getResource(map[baseURI.toString()]).readText()
+        }
+
+        override fun getContent(path: String): String {
+            val candidate = URI(path)
+            val url = if (candidate.isAbsolute) candidate else baseURI.resolve(candidate)
+            val resource = map[url.toString()].toString()
+            return this::class.java.getResource(resource).readText()
+        }
+
+        override fun withPath(path: String): ResourceLocator {
+            val candidate = URI(path)
+            val url = if (candidate.isAbsolute) candidate else baseURI.resolve(candidate)
+            return TestResourceLocatorURL(url)
+        }
+
+    }
+
     @Test
     fun test() {
-        val resource = "/datapackage-paysage.json"
+        val resource = "/budget.json"
         val rl = TestResourceLocator(resource)
         val model = conv.readString(resource, rl)
         println(ModelHumanPrinterEmoji().print(model))
+        // TODO complete test
+    }
+
+    @Test
+    fun testDataPackage() {
+        val resource = URI("https://raw.githubusercontent.com/cnigfr/schema-paysage/refs/heads/main/datapackage.json")
+        val locator = TestResourceLocatorURL(resource)
+        val model = conv.readString(resource.toString(), locator)
+        println(ModelHumanPrinterEmoji().print(model))
+        // TODO complete test
     }
 }
