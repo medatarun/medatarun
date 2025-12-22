48bfcffad307b76e54ad2946ccbfcfee84d38dc0
Sébastien Just <sebastien.just@seij.net>
2025-12-22 18:52:55 +0100

    [ui] Entity overview separated

---
+++ ui/src/components/business/EntityOverview.tsx
@@ -0,0 +1,52 @@
+import {Caption1, Text} from "@fluentui/react-components";
+import {ExternalUrl, Markdown, Origin} from "../../views/ModelPage.tsx";
+import {Link} from "@tanstack/react-router";
+import {Tags} from "../core/Tag.tsx";
+import type {EntityDto} from "../../business/model.tsx";
+
+
+export function EntityOverview({entity}: { entity: EntityDto}) {
+  return <div>
+    <div style={{
+      float: "right",
+      display: "float",
+      border: "1px solid #ccc",
+      padding: "1em",
+      width: "20em",
+      overflow: "hidden"
+    }}>
+      <div>
+        <div style={{marginBottom: "1em"}}>
+          <div><Caption1>Key</Caption1></div>
+          <div><Text><code>{entity.id}</code></Text></div>
+        </div>
+        <div style={{marginBottom: "1em"}}>
+          <div><Caption1>Model</Caption1></div>
+          <div><Link to="/model/$modelId"
+                     params={{modelId: entity.model.id}}>{entity.model.name ?? entity.model.id}</Link></div>
+        </div>
+        <div style={{marginBottom: "1em"}}>
+          <div><Caption1>External documentation</Caption1></div>
+          <div><ExternalUrl url={entity.documentationHome}/></div>
+        </div>
+        <div style={{marginBottom: "1em"}}>
+          <div><Caption1>Tags</Caption1></div>
+          <div><Tags tags={entity.hashtags}/></div>
+        </div>
+        <div style={{marginBottom: "1em"}}>
+          <div><Caption1>Origin</Caption1></div>
+          <div><Origin value={entity.origin}/></div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <Markdown value={entity.description}/>
+      <div>
+        <Caption1 truncate style={{overflow: "hidden", width: "30em", display: "block",}}
+                  wrap={false}>{entity.attributes.map(it => (it.id ?? it.name)).join(", ")}</Caption1>
+      </div>
+    </div>
+
+  </div>
+
+}
--- ui/src/views/EntityPage.tsx
+++ ui/src/views/EntityPage.tsx
@@ -1,21 +1,17 @@
 import {useEffect, useState} from "react";
 import {Link, useNavigate} from "@tanstack/react-router";
-import {ExternalUrl, Markdown, Origin} from "./ModelPage.tsx";
 import {type EntityDto, Model, type ModelDto} from "../business/model.tsx";
 import {ModelContext, useModelContext} from "../components/business/ModelContext.tsx";
-import {Tags} from "../components/core/Tag.tsx";
 import {ViewTitle} from "../components/core/ViewTitle.tsx";
 import {
   Breadcrumb,
   BreadcrumbButton,
   BreadcrumbDivider,
   BreadcrumbItem,
-  Caption1,
   Divider,
   Tab,
   TabList,
-  type TabValue,
-  Text
+  type TabValue
 } from "@fluentui/react-components";
 import {AttributeIcon, EntityIcon, ModelIcon, RelationshipIcon} from "../components/business/Icons.tsx";
 import {AttributesTable} from "../components/business/AttributesTable.tsx";
@@ -24,6 +20,7 @@ import {InfoRegular} from "@fluentui/react-icons";
 import {TabPanel} from "../components/core/TabPanel.tsx";
 import {ViewLayoutContained} from "../components/layout/ViewLayoutContained.tsx";
 import {ActionsBar} from "../components/business/ActionsBar.tsx";
+import {EntityOverview} from "../components/business/EntityOverview.tsx";
 
 
 export function EntityPage({modelId, entityDefId}: { modelId: string, entityDefId: string }) {
@@ -83,50 +80,7 @@ export function EntityView({entity}: { entity: EntityDto }) {
       <div style={{flexGrow: 1, overflowY: "auto"}}>
         {selectedTab === "info" && (
           <TabPanel>
-            <div>
-              <div style={{
-                float: "right",
-                display: "float",
-                border: "1px solid #ccc",
-                padding: "1em",
-                width: "20em",
-                overflow: "hidden"
-              }}>
-                <div>
-                  <div style={{marginBottom: "1em"}}>
-                    <div><Caption1>Key</Caption1></div>
-                    <div><Text><code>{entity.id}</code></Text></div>
-                  </div>
-                  <div style={{marginBottom: "1em"}}>
-                    <div><Caption1>Model</Caption1></div>
-                    <div><Link to="/model/$modelId"
-                               params={{modelId: entity.model.id}}>{entity.model.name ?? entity.model.id}</Link></div>
-                  </div>
-                  <div style={{marginBottom: "1em"}}>
-                    <div><Caption1>External documentation</Caption1></div>
-                    <div><ExternalUrl url={entity.documentationHome}/></div>
-                  </div>
-                  <div style={{marginBottom: "1em"}}>
-                    <div><Caption1>Tags</Caption1></div>
-                    <div><Tags tags={entity.hashtags}/></div>
-                  </div>
-                  <div style={{marginBottom: "1em"}}>
-                    <div><Caption1>Origin</Caption1></div>
-                    <div><Origin value={entity.origin}/></div>
-                  </div>
-                </div>
-              </div>
-              <div>
-                <Markdown value={entity.description}/>
-                <div>
-                  <Caption1 truncate style={{overflow: "hidden", width: "30em", display: "block",}}
-                            wrap={false}>{entity.attributes.map(it => (it.id ?? it.name)).join(", ")}</Caption1>
-                </div>
-              </div>
-
-            </div>
-
-
+            <EntityOverview entity={entity}/>
           </TabPanel>
         )}
         {selectedTab == "attributes" && (
