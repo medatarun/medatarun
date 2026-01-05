a0e68ac79e6acb04702c1d69614eb9f138622493
Sebastien Just <sebastien.just@seij.net>
2026-01-05 15:45:20 +0100

    [cli] no logs in CLI, only in server mode (#7)

--- app/src/main/kotlin/io/medatarun/App.kt
+++ app/src/main/kotlin/io/medatarun/App.kt
@@ -1,31 +1,49 @@
 package io.medatarun
 
 import io.medatarun.cli.AppCLIRunner
+import io.medatarun.cli.AppCLIUtils.configureCliLogging
+import io.medatarun.cli.AppCLIUtils.isServerMode
 import io.medatarun.httpserver.AppHttpServer
 import io.medatarun.runtime.internal.AppRuntimeBuilder
+import io.medatarun.runtime.internal.AppRuntimeConfig
 import io.medatarun.runtime.internal.AppRuntimeConfigFactory
-import org.slf4j.LoggerFactory
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main(args: Array<String>) {
 
-    val config = AppRuntimeConfigFactory().create()
-    val serverPort: Int = config.getProperty("medatarun.server.port", "8080").toInt()
-    val serverHost: String = config.getProperty("medatarun.server.host", "0.0.0.0")
+    val isServerMode = isServerMode(args)
 
-    if (args.isNotEmpty() && args[0] == "serve") {
-        val runtime = AppRuntimeBuilder(config).build()
+    if (isServerMode) {
+        val c = createConfig()
+        val runtime = AppRuntimeBuilder(c.config).build()
         AppHttpServer(runtime).start(
-            host = serverHost,
-            port = serverPort,
+            host = c.serverHost,
+            port = c.serverPort,
             wait = true
         )
     } else {
-        val logger = LoggerFactory.getLogger("MAIN")
-        logger.info("Connecting to $serverHost:$serverPort")
-        val cliRunner = AppCLIRunner(args, defaultServerHost = serverHost, defaultServerPort = serverPort)
+        configureCliLogging()
+        val c = createConfig()
+        val cliRunner = AppCLIRunner(args, defaultServerHost = c.serverHost, defaultServerPort = c.serverPort)
         cliRunner.handleCLI()
     }
 
+}
+
+fun createConfig(): BasicConfig {
+    val config = AppRuntimeConfigFactory().create()
+    val serverPort: Int = config.getProperty("medatarun.server.port", "8080").toInt()
+    val serverHost: String = config.getProperty("medatarun.server.host", "0.0.0.0")
+    return object : BasicConfig {
+        override val config: AppRuntimeConfig = config
+        override val serverHost: String = serverHost
+        override val serverPort: Int = serverPort
+    }
+}
+
+interface BasicConfig {
+    val config: AppRuntimeConfig
+    val serverHost: String
+    val serverPort: Int
 }
