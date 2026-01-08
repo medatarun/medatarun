607a47fe0490552cd307cd1fb4d616044a3466f5
Sebastien Just <sebastien.just@seij.net>
2026-01-08 12:47:04 +0100

    Relocation of packages

--- libs/model/src/main/kotlin/io/medatarun/actions/ports/needs/ActionPrincipalCtx.kt
+++ libs/model/src/main/kotlin/io/medatarun/actions/ports/needs/ActionPrincipalCtx.kt
@@ -3,6 +3,5 @@ package io.medatarun.actions.ports.needs
 interface ActionPrincipalCtx {
     fun ensureIsAdmin()
     fun ensureSignedIn(): MedatarunPrincipal
-    fun ensureIsEmbeddedUser(): MedatarunPrincipal
     val actor: MedatarunPrincipal?
 }
