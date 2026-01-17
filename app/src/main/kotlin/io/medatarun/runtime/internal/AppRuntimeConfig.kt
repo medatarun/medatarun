5c377ec06cddd794445634ec30615101270639a0
Sebastien Just <sebastien.just@seij.net>
2026-01-17 14:47:10 +0100

    Removed config logger

--- app/src/main/kotlin/io/medatarun/runtime/internal/AppRuntimeConfig.kt
+++ app/src/main/kotlin/io/medatarun/runtime/internal/AppRuntimeConfig.kt
@@ -3,7 +3,6 @@ package io.medatarun.runtime.internal
 import io.medatarun.platform.kernel.MedatarunConfig
 import io.medatarun.platform.kernel.ResourceLocator
 import org.eclipse.microprofile.config.Config
-import org.slf4j.LoggerFactory
 import java.nio.file.Path
 
 class AppRuntimeConfig(
@@ -25,10 +24,4 @@ class AppRuntimeConfig(
         return config.getOptionalValue(key, String::class.java).orElse(defaultValue)
     }
 
-    init {
-        val logger = LoggerFactory.getLogger("CONFIG")
-        config.propertyNames.sorted().forEach {
-            logger.info(it + "=" + config.getOptionalValue(it, String::class.java))
-        }
-    }
 }
