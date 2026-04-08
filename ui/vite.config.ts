ff34303e95ddf2c1ac864bd92236f59fd6afecec
Sébastien Just <sebastien.just@seij.net>
2026-04-08 12:28:23 +0200

    chore(deps): bum vite to version 8

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "node:path";
 
 // https://vite.dev/config/
@@ -11,20 +11,38 @@ export default defineConfig({
     },
   },
   build: {
-    rollupOptions: {
+    rolldownOptions: {
       input: {
         main: path.resolve(__dirname, "index.html"),
         login: path.resolve(__dirname, "login.html"),
       },
       output: {
-        manualChunks(id) {
-          if (
-            id.includes("@seij/common-ui") ||
-            id.includes("@seij/common-ui-auth")
-          ) {
-            return "seij-common-ui";
-          }
-          return undefined;
+        codeSplitting: {
+          groups: [
+            {
+              name(id) {
+                if (
+                  id.includes("@seij/common-ui") ||
+                  id.includes("@seij/common-ui-auth")
+                ) {
+                  return "seij-common-ui";
+                }
+                return null;
+
+              }
+            },
+            {
+              name(id) {
+                if (
+                  id.includes("node_modules")
+                ) {
+                  return "vendor";
+                }
+                return null
+              },
+            },
+
+          ]
         },
       },
     },
