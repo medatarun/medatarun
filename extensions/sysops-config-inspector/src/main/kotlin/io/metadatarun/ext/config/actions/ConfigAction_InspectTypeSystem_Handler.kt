730b5596c28b062a3e18dc09c43a3157dc41d9b2
Sebastien Just <sebastien.just@seij.net>
2026-05-25 09:28:43 +0200

    features/typesystem enum values (#194)
    * [config] typesystem returns enum values
    
    * [config] typesystem returns enum values

--- extensions/sysops-config-inspector/src/main/kotlin/io/metadatarun/ext/config/actions/ConfigAction_InspectTypeSystem_Handler.kt
+++ extensions/sysops-config-inspector/src/main/kotlin/io/metadatarun/ext/config/actions/ConfigAction_InspectTypeSystem_Handler.kt
@@ -2,6 +2,7 @@ package io.metadatarun.ext.config.actions
 
 import io.medatarun.actions.ports.needs.ActionCtx
 import io.medatarun.platform.kernel.ExtensionRegistry
+import io.medatarun.type.commons.enums.EnumWithCode
 import io.medatarun.types.TypeDescriptor
 import kotlinx.serialization.Serializable
 
@@ -15,10 +16,29 @@ class ConfigAction_InspectTypeSystem_Handler(
                 TypeDescriptorDto(
                     id = it.equivMultiplatorm,
                     equivJson = it.equivJson.code,
-                    description = it.description
+                    description = it.description,
+                    enumValues = enumValues(it)
                 )
             }
         )
+    }
+
+    private fun enumValues(typeDescriptor: TypeDescriptor<*>): List<String>? {
+        val targetJavaClass = typeDescriptor.target.java
+        if (!targetJavaClass.isEnum) {
+            return null
+        }
+
+        val enumConstants = targetJavaClass.enumConstants
+        if (EnumWithCode::class.java.isAssignableFrom(targetJavaClass)) {
+            return enumConstants.map { enumConstant ->
+                (enumConstant as EnumWithCode).code
+            }
+        }
+
+        return enumConstants.map { enumConstant ->
+            (enumConstant as Enum<*>).name
+        }
     }
 }
 
@@ -29,5 +49,6 @@ data class InspectTypeSystemResp(val items: List<TypeDescriptorDto>)
 data class TypeDescriptorDto(
     val id: String,
     val equivJson: String,
-    val description: String
+    val description: String,
+    val enumValues: List<String>?
 )
